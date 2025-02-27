26b4f5b2f590d4ce60d8a781a5090f951950c1c1
CleilsonAndrade <cleilsonjose@hotmail.com>
2025-02-27 17:05:20 -0300

    build: watching with pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: 'carteira_vacinacao',
       script: './src/server.js',
+      watch: true,
+      ignore_watch: ['node_modules'],
       instances: 1,
       exec_mode: 'cluster',
     },
